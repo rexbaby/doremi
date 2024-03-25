5853c828ce82e328d4cc6f44b59610e257d35e62
Nene <tina@pollex.com.tw>
2024-03-25 22:31:45 +0800

    [ Update ] 假資料透過service提供

--- src/app/features/organization/dept.service.ts
+++ src/app/features/organization/dept.service.ts
@@ -3,33 +3,115 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DeptService {
+    mock_region_data: RegionDTO[] = [
+        { id: 1, region: '北部區域', createDate: new Date() },
+        { id: 2, region: '中部區域', createDate: new Date() },
+        { id: 3, region: '南部區域', createDate: new Date() },
+        { id: 4, region: '東部區域', createDate: new Date() },
+    ];
 
-constructor(
-  // private http: HttpClient
-) { }
+    mock_Agent_List_By_Region: AgentDTO[] = [
+        {
+            region: '北部地區',
+            agentCode: 'A001',
+            name: 'aaa',
+            deptCode: '10010',
+            gender: 'male',
+            phone: '09111222333',
+            title: '業務員',
+            boardingDate: new Date(),
+            status: '有效',
+        },
+        {
+            region: '北部地區',
+            agentCode: 'B001',
+            name: 'bbb',
+            deptCode: '99999',
+            gender: 'female',
+            phone: '09111222333',
+            title: '區經理',
+            boardingDate: new Date(),
+            status: '有效',
+        },
+        {
+            region: '北部地區',
+            agentCode: 'C001',
+            name: 'ccc',
+            deptCode: '10010',
+            gender: 'male',
+            phone: '09111222333',
+            title: '區經理',
+            boardingDate: new Date(),
+            status: '有效',
+        },
+        {
+            region: '北部地區',
+            agentCode: 'D001',
+            name: 'ddd',
+            deptCode: '10010',
+            gender: 'male',
+            phone: '09111222333',
+            title: '業務員',
+            boardingDate: new Date(),
+            status: '有效',
+        },
+    ]
 
-  /**
-   * 新增區域
-   * @param regionName 
-   * @returns 
-   */
-  addDept(regionName: string): Observable<RegionDTO> {
-    const fakeResult: RegionDTO = { id: 10, region: regionName, createDate: new Date()}
-    return of(fakeResult);
-  }
+    constructor(
+        // private http: HttpClient
+    ) { }
 
+    /**
+     * 取得區域清單
+     * @returns 
+     */
+    getRegionList(): Observable<RegionDTO[]> {
+        return of(this.mock_region_data);
+    }
 
-  modifyDept(id: number, regionName: string): Observable<RegionDTO> {
-    const fakeResult: RegionDTO = { id: id, region: regionName, createDate: new Date()}
-    return of(fakeResult);
-  }
+    /**
+     * 新增區域
+     * @param regionName 
+     * @returns 
+     */
+    addRegion(regionName: string): Observable<RegionDTO> {
+        const fakeResult: RegionDTO = { id: 10, region: regionName, createDate: new Date() }
+        return of(fakeResult);
+    }
+
+    /**
+     * 編輯區域
+     * @param id 
+     * @param regionName 
+     * @returns 
+     */
+    modifyRegion(id: number, regionName: string): Observable<RegionDTO> {
+        const fakeResult: RegionDTO = { id: id, region: regionName, createDate: new Date() }
+        return of(fakeResult);
+    }
+
+    getAgentList(deptId: number): Observable<AgentDTO[]> {
+        return of(this.mock_Agent_List_By_Region);
+    }
 }
 
 export interface RegionDTO {
-  id: number;
-  region: string;
-  createDate: Date;
+    id: number;
+    region: string;
+    createDate: Date;
+}
+
+export interface AgentDTO {
+    region: string; // 用在新增/編輯人員DIALOG顯示
+    agentCode: string;
+    name: string;
+    deptCode: string;
+    gender: string;
+    phone: string;
+    title: string;
+    boardingDate: Date;
+    status: string;
 }
--- src/app/features/organization/organization.model.ts
+++ src/app/features/organization/organization.model.ts
@@ -1,5 +1,5 @@
 import { TableLayout } from "src/app/shared/element-ui/table/table.model";
-import { RegionDTO } from "./dept.service";
+import { AgentDTO, RegionDTO } from "./dept.service";
 
 export const OrgTableLayout: TableLayout[] = [
     { label: '區域', colKey: 'region', cusCssClass: 'w-20' },
@@ -7,21 +7,29 @@ export const OrgTableLayout: TableLayout[] = [
     { label: '動作', colKey: 'action', cusCssClass: 'w-50' },
 ];
 
-export interface RegionTableVM extends RegionDTO {
-    action: string;
-}
-
-export const ELEMENT_DATA: RegionTableVM[] = [
-    { id: 1, region: '北部區域', createDate: new Date(), action: '' },
-    { id: 2, region: '中部區域', createDate: new Date(), action: '' },
-    { id: 3, region: '南部區域', createDate: new Date(), action: '' },
-    { id: 4, region: '東部區域', createDate: new Date(), action: '' },
+export const AngentsTableLayout: TableLayout[] = [
+    { label: '姓名', colKey: 'name', cusCssClass: 'w-8' },
+    { label: '代號', colKey: 'agentCode', cusCssClass: 'w-8' },
+    { label: '處代號', colKey: 'deptCode', cusCssClass: 'w-8' },
+    { label: '性別', colKey: 'gender', cusCssClass: 'w-8' },
+    { label: '電話', colKey: 'phone', cusCssClass: 'w-10' },
+    { label: '職稱', colKey: 'title', cusCssClass: 'w-8' },
+    { label: '入職日期', colKey: 'boardingDate', cusCssClass: 'w-10' },
+    { label: '狀態', colKey: 'status', cusCssClass: 'w-7' },
+    { label: '動作', colKey: 'action', cusCssClass: 'w-15' },
 ];
 
 export interface RegionDialog {
     state: string;
     region: RegionDTO | undefined;
 }
+
+export interface AgentDialog {
+    state: string;
+    region: string;
+    agent: AgentDTO | undefined;
+} 
+// export type AgentDialog = Pick<AgentDTO, "region" | "deptCode" | "name" | "gender" | "phone" | "boardingDate" | "status">;
 
 export enum DIALOG_STATE {
     EDIT = 'edit',
